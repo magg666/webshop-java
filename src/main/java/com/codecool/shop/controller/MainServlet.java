e79498cca9e21757a400beb258f628201e634d86
magg666 <mw23127@gmail.com>
2019-06-03 23:40:05 +0200

    added generating variables in servlet. added displaying by department & category

--- src/main/java/com/codecool/shop/controller/MainServlet.java
+++ src/main/java/com/codecool/shop/controller/MainServlet.java
@@ -1,11 +1,6 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.implementation.CartDaoMem;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -14,37 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MainServlet extends HttpServlet {
-    WebContext context;
 
 
-
-    void renderTemplate(HttpServletRequest req, HttpServletResponse resp, String template, String notYetKnown) throws ServletException, IOException {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        ProductDao cart = CartDaoMem.getInstance();
-
-
-//        Map params = new HashMap<>();
-//        params.put("category", productCategoryDataStore.find(1));
-//        params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-
+    void renderTemplate(HttpServletRequest req, HttpServletResponse resp, String template, Map<String, Object> params) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-
-        context.setVariable("category", productCategoryDataStore.getAll());
-        context.setVariable("categoryObject", productCategoryDataStore);
-        context.setVariable("departments", productCategoryDataStore.getAllDepartments());
-        context.setVariable("products", productDataStore.getAll());
-        context.setVariable("cart", cart);
-        context.setVariable("productsByDepartments", productCategoryDataStore.getByDepartments(notYetKnown));
-
+        context.setVariables(params);
         engine.process(template, context, resp.getWriter());
     }
-
-//    void setVariable(String name, Object object){
-//        getContext().get;
-//
-//    }
 }
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,36 +1,62 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.CartDaoMem;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Product;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends MainServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String productId = req.getParameter("product_id");
-        String department = req.getParameter("department");
+        // get necessary instances
         ProductDao cart = CartDaoMem.getInstance();
         ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
 
-        if(productId != null){
+        // define variables
+        Map<String, Object> params = new HashMap<>();
+        params.put("departments", productCategoryDataStore.getAllDepartments());
+        params.put("products", productDataStore.getAll());
+        params.put("cart", cart);
+
+        // define parameters for template
+        String productId = req.getParameter("product_id");
+        String department = req.getParameter("department");
+        String productsCategory = req.getParameter("cat");
+
+        // adding to cart
+        if (productId != null) {
             cart.add(productDataStore.find(Integer.parseInt(productId)));
         }
-        if(department != null){
-            renderTemplate(req, resp, "/categoryPage.html", department);
+
+        // searching page by departments and categories
+        if (department == null) {
+            renderTemplate(req, resp, "/store.html", params);
         } else {
-            renderTemplate(req,resp,"/store.html", null);
+            if (productsCategory != null) {
+                int categoryId = Integer.parseInt(productsCategory);
+                List <Product> productsByCategory = productCategoryDataStore.find(categoryId).getProducts();
+                params.put("category", productCategoryDataStore.find(categoryId).getName());
+                params.put("productsByCategories", productsByCategory);
+                renderTemplate(req, resp, "/categoryTemplate.html", params);
+            } else {
+                params.put("categoriesInDepartments", productCategoryDataStore.getByDepartments(department));
+                params.put("productsByDepartments", productDataStore.getByDepartments(department));
+                renderTemplate(req, resp, "/departmentsTemplate.html", params);
+            }
         }
-
-
-
     }
-
 }
