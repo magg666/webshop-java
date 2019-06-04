a1d77eb1f6b75328f08350277cf4d0d20f04166a
Michal Kalinowski <subaru.kali@gmail.com>
2019-06-04 19:19:58 +0200

    refactor

--- src/main/java/com/codecool/shop/model/BaseModel.java
+++ src/main/java/com/codecool/shop/model/BaseModel.java
@@ -48,9 +48,8 @@ public class BaseModel {
         final StringBuilder sb = new StringBuilder();
         for (Field field : this.getClass().getDeclaredFields()) {
             field.setAccessible(true);
-            Object value = null;
             try {
-                value = field.get(this);
+                Object value = field.get(this);
                 if (value != null) {
                     sb.append(field.getName() + ":" + value + ",");
                 }
