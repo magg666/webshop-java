d6865f9d7394161adfefe544ca1b9211e1b3d8da
magg666 <mw23127@gmail.com>
2019-06-06 01:22:39 +0200

    added payment post request

--- src/main/java/com/codecool/shop/controller/PaymentServlet.java
+++ src/main/java/com/codecool/shop/controller/PaymentServlet.java
@@ -5,11 +5,27 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @WebServlet(urlPatterns = {"/payment"})
 public class PaymentServlet extends MainServlet {
+
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        renderTemplate(req, resp, "/confirmationPage.html", null);
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, Object> orderVariables = makeMapOfOrderVariables();
+        renderTemplate(req, resp, "/paymentTemplate.html", orderVariables);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        String payment = req.getParameter("payment");
+        String terms = req.getParameter("terms");
+        System.out.println(payment);
+        System.out.println(terms);
+
+        sendMail(req, resp);
+        resp.sendRedirect("/thank-you");
     }
 }
