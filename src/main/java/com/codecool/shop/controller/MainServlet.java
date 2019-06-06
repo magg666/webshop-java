9e73b3334b1a9cde358275ed88189c3953b02e06
magg666 <mw23127@gmail.com>
2019-06-06 21:21:49 +0200

    added sending email to parameter

--- src/main/java/com/codecool/shop/controller/MainServlet.java
+++ src/main/java/com/codecool/shop/controller/MainServlet.java
@@ -14,7 +14,6 @@ import org.thymeleaf.context.WebContext;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -65,13 +64,10 @@ public class MainServlet extends HttpServlet {
     }
 
 
-    public void sendMail(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
+    public void sendMail(String customerEmail) {
         try {
             final String fromEmail = "webshopcodecool@gmail.com"; //requires valid gmail id
             final String password = "codecool666"; // correct password for gmail id
-            final String toEmail = "mw23127@gmail.com"; // can be any email id
 
             System.out.println("TLSEmail Start");
             Properties props = new Properties();
@@ -91,17 +87,12 @@ public class MainServlet extends HttpServlet {
 
             MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(fromEmail));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(customerEmail));
 
-            System.out.println("Mail Check 2");
-
-            message.setSubject("another message");
-            message.setText("hello world");
-
-            System.out.println("Mail Check 3");
+            message.setSubject("Your order in Hot Cart");
+            message.setText("You ordered things, thank you");
 
             Transport.send(message);
-            System.out.println("Mail Sent");
         } catch (Exception ex) {
             System.out.println("Mail fail");
             System.out.println(ex);
