35a89d8173602e40873672c8b56a21c39f6f5710
magg666 <mw23127@gmail.com>
2019-06-06 21:22:28 +0200

    refactor

--- src/main/java/com/codecool/shop/controller/PaymentServlet.java
+++ src/main/java/com/codecool/shop/controller/PaymentServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Map;
 
@@ -40,30 +41,41 @@ public class PaymentServlet extends MainServlet {
         String[] paypalData = new String[]{paypalMail, paypalPassword};
         String[] cardData = new String[]{cardOwner, cardCvv, cardNumber, cardExpMonth, cardExpYear};
 
+        HttpSession session = req.getSession();
+        String customerEmail = (String) session.getAttribute("email");
 
-        //compare selected value
-        if ("direct".equals(paymentMethod)) {
-            order.getOrder().setPaymentMethod("direct");
-            sendMail(req, resp);
+        if (util.isPaymentAndTermsChecked(paymentMethod, terms) && util.isChosenPaymentFilled(paymentMethod, cardData, paypalData)) {
+            order.setPayment(paymentMethod);
+            sendMail(customerEmail);
             resp.sendRedirect("/thank-you");
-            System.out.println("payment method");
-            System.out.println(order.getOrder().getPaymentMethod());
-        } else if ("card".equals(paymentMethod)) {
-            if (util.isNotNull(cardData)) {
-                order.getOrder().setPaymentMethod("card");
-                sendMail(req, resp);
-                resp.sendRedirect("/thank-you");
-            }
-        } else if ("paypal".equals(paymentMethod)) {
-            if (util.isNotNull(paypalData)) {
-                order.getOrder().setPaymentMethod("paypal");
-                sendMail(req, resp);
-                resp.sendRedirect("/thank-you");
-            }
         } else {
             resp.sendRedirect("/payment");
         }
-//
-//        renderTemplate(req, resp, "/paymentTemplate.html", null);
+
+
+        //compare selected value
+//        if ("direct".equals(paymentMethod)) {
+//            order.getOrder().setPaymentMethod("direct");
+//            sendMail(customerEmail);
+//            resp.sendRedirect("/thank-you");
+//        } else if ("card".equals(paymentMethod)) {
+//            if (util.isNotNull(cardData)) {
+//                order.getOrder().setPaymentMethod("card");
+//                sendMail(customerEmail);
+//                resp.sendRedirect("/thank-you");
+//            }
+//        } else if ("paypal".equals(paymentMethod)) {
+//            if (util.isNotNull(paypalData)) {
+//                order.getOrder().setPaymentMethod("paypal");
+//                sendMail(customerEmail);
+//                resp.sendRedirect("/thank-you");
+//            }
+//        } else {
+//            System.out.println(paymentMethod);
+//            String message = "Unfortunately you didn't put right values in fields. Insert correct data to continue";
+//            Map<String, Object> additionalVariables = new HashMap<>();
+//            additionalVariables.put("message", message);
+//            renderTemplate(req, resp, "/paymentTemplate.html", additionalVariables);
+//        }
     }
 }
