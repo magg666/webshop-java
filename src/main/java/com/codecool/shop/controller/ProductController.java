eb4ee1ae14a828cc742c0ae5ed4ae7eff5b77cba
magg666 <mw23127@gmail.com>
2019-06-19 00:56:48 +0200

    refactored get method

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,42 +1,56 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.db_implementation.ProductCategoryDaoJDBC;
+import com.codecool.shop.dao.db_implementation.ProductDaoJDBC;
 import com.codecool.shop.dao.implementation.OrderDaoMem;
+import com.codecool.shop.service.PageCoordinator;
+import com.codecool.shop.service.SessionManager;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends MainServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        OrderDao order = OrderDaoMem.getInstance();
+        ProductDao products = new ProductDaoJDBC();
+        ProductCategoryDao categories = new ProductCategoryDaoJDBC();
 
-        // define custom variables
-        Map<String, Object> additionalVariables = new HashMap<>();
-        additionalVariables.put("cart", order.getOrder());
+        OrderDao currentOrder = OrderDaoMem.getInstance();
+        SessionManager.setOrderInSession(req, currentOrder);
 
-        // define parameters for template
-//        String productId = req.getParameter("product_id");
-        String department = req.getParameter("department");
-        String productsCategory = req.getParameter("cat");
-
-//        // adding to cart
-//        if (productId != null) {
-//            order.addById(Integer.parseInt(productId));
-//        }
-
-        // searching page by departments and categories
-        if (department == null) {
-            renderTemplate(req, resp, "/store.html", additionalVariables);
+        if(PageCoordinator.hasUserWantedToLookAround(req)){
+            PageCoordinator.navigateAmongProducts(req, resp, products, categories);
         } else {
-            composeProductsDivision(req, resp, additionalVariables, department, productsCategory);
+            PageCoordinator.goToRequestedPage(req, resp, "/storeTemplate.html", products, categories);
         }
+//
+//        // define custom variables
+//        Map<String, Object> additionalVariables = new HashMap<>();
+//        additionalVariables.put("cart", order.getOrder());
+//
+//        // define parameters for template
+////        String productId = req.getParameter("product_id");
+//        String department = req.getParameter("department");
+//        String productsCategory = req.getParameter("cat");
+//
+////        // adding to cart
+////        if (productId != null) {
+////            order.addById(Integer.parseInt(productId));
+////        }
+//
+//        // searching page by departments and categories
+//        if (department == null) {
+//            renderTemplate(req, resp, "/store.html", additionalVariables);
+//        } else {
+//            composeProductsDivision(req, resp, additionalVariables, department, productsCategory);
+//        }
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
