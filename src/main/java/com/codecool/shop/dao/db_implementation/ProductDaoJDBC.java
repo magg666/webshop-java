45ce4a29e36ed91138719635317b92edef7bca2e
magg666 <mw23127@gmail.com>
2019-06-19 00:58:45 +0200

    added to query id of product added array list initialization

--- src/main/java/com/codecool/shop/dao/db_implementation/ProductDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/db_implementation/ProductDaoJDBC.java
@@ -11,10 +11,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDaoJDBC implements ProductDao {
-    private DataBaseConfiguration dataBaseConfiguration;
+    private DataBaseConfiguration dataBaseConfiguration = new DataBaseConfiguration();
 
     public ProductDaoJDBC() {
     }
@@ -35,33 +36,29 @@ public class ProductDaoJDBC implements ProductDao {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
     public Product find(int id) {
-        String query = "SELECT p.name, p.price, p.currency, p.description, c.name cat_name, d.name dep_name, s.id sup_id, s.name sup_name " +
+        String query = "SELECT p.id, p.name, p.price, p.currency, p.description, " +
+                "c.id as cat_id, c.name as cat_name, d.name as dep_name, " +
+                "s.id sup_id, s.name sup_name " +
                 "FROM products p " +
                 "LEFT JOIN categories c on p.category_id = c.id " +
                 "LEFT JOIN suppliers s on p.supplier_id = s.id " +
                 "LEFT JOIN departments d on c.department_id = d.id " +
                 "WHERE p.id = ?;";
-        Product product = null;
         try (Connection connection = dataBaseConfiguration.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
-                product = createProductFromDatabase(resultSet);
-            } else {
-                return null;
+                return createProductFromDatabase(resultSet);
             }
-
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return product;
+        return null;
     }
 
     @Override
@@ -80,12 +77,14 @@ public class ProductDaoJDBC implements ProductDao {
 
     @Override
     public List<Product> getAll() {
-        String query = "SELECT p.name, p.price, p.currency, p.description, c.name cat_name, d.name dep_name, s.id sup_id, s.name sup_name " +
+        String query = "SELECT p.id, p.name, p.price, p.currency, p.description, " +
+                "c.id as cat_id, c.name as cat_name, d.name as dep_name, " +
+                "s.id sup_id, s.name sup_name " +
                 "FROM products p " +
                 "LEFT JOIN categories c on p.category_id = c.id " +
                 "LEFT JOIN suppliers s on p.supplier_id = s.id " +
-                "LEFT JOIN departments d on c.department_id = d.id";
-        List<Product> productsList = null;
+                "LEFT JOIN departments d on c.department_id = d.id ";
+        List<Product> productsList = new ArrayList<>();
         try (Connection connection = dataBaseConfiguration.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
             ResultSet resultSet = statement.executeQuery();
@@ -101,7 +100,9 @@ public class ProductDaoJDBC implements ProductDao {
 
     @Override
     public List<Product> getBy(Supplier supplier) {
-        String query = "SELECT p.name, p.price, p.currency, p.description, c.name cat_name, d.name dep_name, s.id sup_id, s.name sup_name " +
+        String query = "SELECT p.id, p.name, p.price, p.currency, p.description, " +
+                "c.id as cat_id, c.name as cat_name, d.name as dep_name, " +
+                "s.id sup_id, s.name sup_name " +
                 "FROM products p " +
                 "LEFT JOIN categories c on p.category_id = c.id " +
                 "LEFT JOIN suppliers s on p.supplier_id = s.id " +
@@ -112,24 +113,28 @@ public class ProductDaoJDBC implements ProductDao {
 
     @Override
     public List<Product> getBy(ProductCategory productCategory) {
-        String query = "SELECT p.name, p.price, p.currency, p.description, c.name cat_name, d.name dep_name, s.id sup_id, s.name sup_name " +
+        String query = "SELECT p.id, p.name, p.price, p.currency, p.description, " +
+                "c.id as cat_id, c.name as cat_name, d.name as dep_name, " +
+                "s.id sup_id, s.name sup_name " +
                 "FROM products p " +
                 "LEFT JOIN categories c on p.category_id = c.id " +
                 "LEFT JOIN suppliers s on p.supplier_id = s.id " +
                 "LEFT JOIN departments d on c.department_id = d.id " +
-                "WHERE c.name = ?";
+                "WHERE c.id = ?";
         return getProducts(productCategory, query);
     }
 
     @Override
     public List<Product> getByDepartments(String department) {
-        String query = "SELECT p.name, p.price, p.currency, p.description, c.name cat_name, d.name dep_name, s.id sup_id, s.name sup_name " +
+        String query = "SELECT p.id, p.name, p.price, p.currency, p.description, " +
+                "c.id as cat_id, c.name as cat_name, d.name as dep_name, " +
+                "s.id sup_id, s.name sup_name " +
                 "FROM products p " +
                 "LEFT JOIN categories c on p.category_id = c.id " +
                 "LEFT JOIN suppliers s on p.supplier_id = s.id " +
                 "LEFT JOIN departments d on c.department_id = d.id " +
                 "WHERE d.name = ?";
-        List<Product> productsList = null;
+        List<Product> productsList = new ArrayList<>();
         try (Connection connection = dataBaseConfiguration.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, department);
@@ -146,21 +151,26 @@ public class ProductDaoJDBC implements ProductDao {
 
     private Product createProductFromDatabase(ResultSet resultSet) throws SQLException {
         return new Product(
+                resultSet.getInt("id"),
                 resultSet.getString("name"),
+                resultSet.getString("description"),
                 resultSet.getFloat("price"),
                 resultSet.getString("currency"),
-                resultSet.getString("description"),
-                new ProductCategory(resultSet.getString("cat_name"),
+                new ProductCategory(
+                        resultSet.getInt("cat_id"),
+                        resultSet.getString("cat_name"),
                         resultSet.getString("dep_name")),
-                new Supplier(resultSet.getInt("sup_id"),
+                new Supplier(
+                        resultSet.getInt("sup_id"),
                         resultSet.getString("sup_name")));
     }
 
     private List<Product> getProducts(BaseModel baseModel, String query) {
         int id = baseModel.getId();
-        List<Product> productsList = null;
+        List<Product> productsList = new ArrayList<>();
         try (Connection connection = dataBaseConfiguration.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
+
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
