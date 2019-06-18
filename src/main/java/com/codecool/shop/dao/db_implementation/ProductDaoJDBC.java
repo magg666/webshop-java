e765479086fb69f3bf6850c6aa7397b3907e6eb4
Michal Kalinowski <subaru.kali@gmail.com>
2019-06-18 10:28:20 +0200

    example

--- src/main/java/com/codecool/shop/dao/db_implementation/ProductDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/db_implementation/ProductDaoJDBC.java
@@ -1,4 +1,51 @@
 package com.codecool.shop.dao.db_implementation;
 
-public class ProductDaoJDBC {
+import com.codecool.shop.config.dbConfig.DataBaseConfiguration;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import java.util.List;
+
+public class ProductDaoJDBC implements ProductDao {
+    private DataBaseConfiguration dataBaseConfiguration;
+
+    public ProductDaoJDBC() {
+    }
+
+    @Override
+    public void add(Product product) {
+
+    }
+
+    @Override
+    public Product find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(Supplier supplier) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(ProductCategory productCategory) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getByDepartments(String department) {
+        return null;
+    }
 }
