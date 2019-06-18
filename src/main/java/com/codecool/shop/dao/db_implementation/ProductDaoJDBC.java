c13c5b165d406b4fb038cde85ad444cd7d3ead70
Michal Kalinowski <subaru.kali@gmail.com>
2019-06-18 19:53:08 +0200

    added all queries

--- src/main/java/com/codecool/shop/dao/db_implementation/ProductDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/db_implementation/ProductDaoJDBC.java
@@ -2,6 +2,7 @@ package com.codecool.shop.dao.db_implementation;
 
 import com.codecool.shop.config.dbConfig.DataBaseConfiguration;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.BaseModel;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
@@ -39,14 +40,19 @@ public class ProductDaoJDBC implements ProductDao {
 
     @Override
     public Product find(int id) {
-        String query = "SELECT * FROM products WHERE id = ?";
+        String query = "SELECT p.name, p.price, p.currency, p.description, c.name cat_name, d.name dep_name, s.id sup_id, s.name sup_name " +
+                "FROM products p " +
+                "LEFT JOIN categories c on p.category_id = c.id " +
+                "LEFT JOIN suppliers s on p.supplier_id = s.id " +
+                "LEFT JOIN departments d on c.department_id = d.id " +
+                "WHERE p.id = ?;";
         Product product = null;
         try (Connection connection = dataBaseConfiguration.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
-//                product = new Product(resultSet.getString("name"), resultSet.getFloat("price"), resultSet.getString("currency"), resultSet.getString("description"));
+                product = createProductFromDatabase(resultSet);
             } else {
                 return null;
             }
@@ -74,36 +80,97 @@ public class ProductDaoJDBC implements ProductDao {
 
     @Override
     public List<Product> getAll() {
-        return null;
+        String query = "SELECT p.name, p.price, p.currency, p.description, c.name cat_name, d.name dep_name, s.id sup_id, s.name sup_name " +
+                "FROM products p " +
+                "LEFT JOIN categories c on p.category_id = c.id " +
+                "LEFT JOIN suppliers s on p.supplier_id = s.id " +
+                "LEFT JOIN departments d on c.department_id = d.id";
+        List<Product> productsList = null;
+        try (Connection connection = dataBaseConfiguration.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Product product = createProductFromDatabase(resultSet);
+                productsList.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productsList;
     }
 
     @Override
     public List<Product> getBy(Supplier supplier) {
-        return null;
+        String query = "SELECT p.name, p.price, p.currency, p.description, c.name cat_name, d.name dep_name, s.id sup_id, s.name sup_name " +
+                "FROM products p " +
+                "LEFT JOIN categories c on p.category_id = c.id " +
+                "LEFT JOIN suppliers s on p.supplier_id = s.id " +
+                "LEFT JOIN departments d on c.department_id = d.id " +
+                "WHERE p.supplier_id = ?";
+        return getProducts(supplier, query);
     }
 
     @Override
     public List<Product> getBy(ProductCategory productCategory) {
-        return null;
+        String query = "SELECT p.name, p.price, p.currency, p.description, c.name cat_name, d.name dep_name, s.id sup_id, s.name sup_name " +
+                "FROM products p " +
+                "LEFT JOIN categories c on p.category_id = c.id " +
+                "LEFT JOIN suppliers s on p.supplier_id = s.id " +
+                "LEFT JOIN departments d on c.department_id = d.id " +
+                "WHERE c.name = ?";
+        return getProducts(productCategory, query);
+    }
+
+    private List<Product> getProducts(BaseModel baseModel, String query) {
+        int id = baseModel.getId();
+        List<Product> productsList = null;
+        try (Connection connection = dataBaseConfiguration.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Product product = createProductFromDatabase(resultSet);
+                productsList.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productsList;
     }
 
     @Override
     public List<Product> getByDepartments(String department) {
-        return null;
+        String query = "SELECT p.name, p.price, p.currency, p.description, c.name cat_name, d.name dep_name, s.id sup_id, s.name sup_name " +
+                "FROM products p " +
+                "LEFT JOIN categories c on p.category_id = c.id " +
+                "LEFT JOIN suppliers s on p.supplier_id = s.id " +
+                "LEFT JOIN departments d on c.department_id = d.id " +
+                "WHERE d.name = ?";
+        List<Product> productsList = null;
+        try (Connection connection = dataBaseConfiguration.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, department);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Product product = createProductFromDatabase(resultSet);
+                productsList.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productsList;
     }
 
-//    private Product createProductFromDatabase(ResultSet resultSet) throws SQLException {
-//        return new Product(
-//                resultSet.getInt("id"),
-//                resultSet.getString("name"),
-//                resultSet.getDouble("price"),
-//                resultSet.getString("currency"),
-//                resultSet.getString("description"),
-//                new ProductCategory(resultSet.getInt("cat_id"),
-//                        resultSet.getString("cat_name"),
-//                        resultSet.getString("dep_name")),
-//                new Supplier(resultSet.getInt("sup_id"),
-//                        resultSet.getString("sup_name")));
-//    }
+    private Product createProductFromDatabase(ResultSet resultSet) throws SQLException {
+        return new Product(
+                resultSet.getString("name"),
+                resultSet.getFloat("price"),
+                resultSet.getString("currency"),
+                resultSet.getString("description"),
+                new ProductCategory(resultSet.getString("cat_name"),
+                        resultSet.getString("dep_name")),
+                new Supplier(resultSet.getInt("sup_id"),
+                        resultSet.getString("sup_name")));
+    }
 
 }
