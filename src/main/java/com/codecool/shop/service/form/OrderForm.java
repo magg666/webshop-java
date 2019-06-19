513607ed6f58ca55fb9d687c5a90000ca26a1384
magg666 <mw23127@gmail.com>
2019-06-20 01:28:39 +0200

    added methods to control order on order page discards product from order, changes quantity of product and saves order

--- src/main/java/com/codecool/shop/service/form/OrderForm.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.shop.service.form;
-
-/**
- * Class for handling cart/order of user on order page
- * It can be used for validation of unfinished orders,
- * removing items from cart or changing its quantity
- */
-public class OrderForm extends Form{
-}
---
+++ src/main/java/com/codecool/shop/util/OrderManager.java
@@ -0,0 +1,58 @@
+package com.codecool.shop.util;
+
+import com.codecool.shop.dao.AllOrdersDao;
+import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.model.Customer;
+
+/**
+ * Class for handling cart/order of user on order page
+ * It can be used for validation of unfinished orders,
+ * removing items from cart or changing its quantity
+ */
+public class OrderManager {
+
+    /**
+     * Method to remove products from order by button
+     * Method validates parameter provided by request - productId. If type of productId is wrong
+     * throws IllegalArgumentException, else remove product from current order
+     *
+     * @param productIdToDecreaseByOne - String, parameter provided by request
+     * @param order - current order
+     */
+    public static void removeFromOrderByTrashButton(String productIdToDecreaseByOne, OrderDao order){
+        if (!Validator.isNumber(productIdToDecreaseByOne)) {
+            throw new IllegalArgumentException(Message.WRONG_TYPE.getMessage());
+        }
+        order.removeProductById(Integer.parseInt(productIdToDecreaseByOne));
+    }
+
+    /**
+     * Method validates and controls inputs from user. This input defines expected quantity of given product
+     * If input is incorrect, method throws IllegalArgumentException
+     *
+     * @param productId - String, parameter provided by request
+     * @param newQuantity - String, parameter provided by request
+     * @param order - current order
+     */
+    public static void changeProductQuantity(String productId, String newQuantity, OrderDao order){
+        if (!Validator.isNumber(newQuantity) || newQuantity.length() > 4) {
+            throw new IllegalArgumentException(Message.WRONG_TYPE.getMessage());
+        }
+        order.changeQuantityOfProduct(Integer.parseInt(productId), Integer.parseInt(newQuantity));
+    }
+
+    /**
+     * Method gets current order, assigns customers to it and saves.
+     * After that clears current order
+     *
+     * @param customer - logged customer
+     * @param currentOrder - current order
+     * @param allOrders - all orders
+     */
+    public static void saveOrder(Customer customer, OrderDao currentOrder, AllOrdersDao allOrders){
+        int customerId = customer.getId();
+            currentOrder.getOrder().setCustomerId(customerId);
+            allOrders.add(currentOrder.getOrder());
+            currentOrder.clearOrder();
+    }
+}
