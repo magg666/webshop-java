4e997899464c4b7e601f72b501ab917eea423e7d
magg666 <mw23127@gmail.com>
2019-06-20 01:33:52 +0200

    added several messages

--- src/main/java/com/codecool/shop/util/Message.java
+++ src/main/java/com/codecool/shop/util/Message.java
@@ -5,4 +5,31 @@ package com.codecool.shop.util;
  * There should be no explicit messages in code, except this class
  */
 public enum Message {
+    WELCOME_REGISTER(" welcome, "),
+    WELCOME_LOGIN(" welcome again, "),
+    SENT_EMAIL(" we have sent you confirmation email. "),
+    EMAIL_INCORRECT_FORMAT(" email is in wrong format "),
+    EMAIL_NOT_EXISTS(" email not exists in our database "),
+    PASSWORD_NOT_MATCH(" password incorrect "),
+    PASSWORDS_NOT_MATCH(" passwords do not match "),
+    EMAIL_EXISTS(" email already exists in our database "),
+    WRONG_LENGTH_OR_NULL(" All fields must be filled. Minimum length of field content - 3 signs "),
+    PHONE_INCORRECT_FORMAT(" incorrect phone number "),
+    ZIP_CODE_INCORRECT_FORMAT(" incorrect zip code "),
+    PASSWORD_EMPTY(" password must not be empty "),
+    ALREADY_LOGGED(" you are already logged "),
+    WRONG_TYPE(" the type of argument is wrong "),
+    EMPTY_CART(" your cart is empty. Maybe put something nice into? "),
+    CART_SAVED(" you successfully saved your cart! You can find it in your account page. ");
+
+
+    private String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+    public String getMessage(){
+        return this.message;
+    }
+
 }
