de5824eb4b24ccaa22d90abb3ad23b4b8bdad281
magg666 <mw23127@gmail.com>
2019-06-19 09:21:53 +0200

    removed coordinator

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -33,14 +33,14 @@
 //        Supplier asus = new Supplier("ASUS", "Wiser together");
 //        Supplier lenovo = new Supplier("Lenovo", "Computers");
 //
-//        supplierDataStore.add(amazon);
-//        supplierDataStore.add(lenovo);
-//        supplierDataStore.add(martin);
-//        supplierDataStore.add(shaefer);
-//        supplierDataStore.add(steel);
-//        supplierDataStore.add(sparks);
-//        supplierDataStore.add(rowling);
-//        supplierDataStore.add(asus);
+//        supplierDataStore.addLineItem(amazon);
+//        supplierDataStore.addLineItem(lenovo);
+//        supplierDataStore.addLineItem(martin);
+//        supplierDataStore.addLineItem(shaefer);
+//        supplierDataStore.addLineItem(steel);
+//        supplierDataStore.addLineItem(sparks);
+//        supplierDataStore.addLineItem(rowling);
+//        supplierDataStore.addLineItem(asus);
 //
 //
 //        //setting up a new product category
@@ -51,28 +51,28 @@
 //        ProductCategory romance = new ProductCategory("Romance", "Books", "Books about love you will never have.");
 //        ProductCategory teens = new ProductCategory("Teens", "Books", "Books for children and teens.");
 //
-//        productCategoryDataStore.add(tablet);
-//        productCategoryDataStore.add(laptop);
-//        productCategoryDataStore.add(router);
-//        productCategoryDataStore.add(sf);
-//        productCategoryDataStore.add(romance);
-//        productCategoryDataStore.add(teens);
+//        productCategoryDataStore.addLineItem(tablet);
+//        productCategoryDataStore.addLineItem(laptop);
+//        productCategoryDataStore.addLineItem(router);
+//        productCategoryDataStore.addLineItem(sf);
+//        productCategoryDataStore.addLineItem(romance);
+//        productCategoryDataStore.addLineItem(teens);
 //
 //        //setting up products and printing it
-//        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-//        productDataStore.add(new Product("Lenovo IdeaPad Mix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-//        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-//        productDataStore.add(new Product("A Game of Thrones", 6.62f, "USD", "Dragons, love and incest", sf, martin));
-//        productDataStore.add(new Product("Fire and Blood", 16.73f, "USD", "A Targaryen History", sf, martin));
-//        productDataStore.add(new Product("Ghosts of Gotham", 5.99f, "USD", "Mystery and magic", sf, shaefer));
-//        productDataStore.add(new Product("Lost and Found", 2.66f, "USD", "Love novel", romance, steel));
-//        productDataStore.add(new Product("Fairytale", 2.99f, "USD", "Love novel again", romance, steel));
-//        productDataStore.add(new Product("The Notebook", 7.88f, "USD", "La La Land was based on this", romance, sparks));
-//        productDataStore.add(new Product("See me", 8.16f, "USD", "Rich in emotion and fueled with suspense", romance, sparks));
-//        productDataStore.add(new Product("Harry Potter", 69.15f, "USD", "The Illustrated Collection", teens, rowling));
-//        productDataStore.add(new Product("ASUS Vivobook K570ZD", 493.37f, "USD", "Full Hd", laptop, asus));
-//        productDataStore.add(new Product("ASUS Whole Home Dual-Band AiMesh Router", 134.99f, "USD", "Mesh Wifi System", router, asus));
-//        productDataStore.add(new Product("ASUS C302CA-DHM4 Chromebook Flip", 465.00f, "USD", "12.5-inch Touchscreen", laptop, asus));
+//        productDataStore.addLineItem(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
+//        productDataStore.addLineItem(new Product("Lenovo IdeaPad Mix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
+//        productDataStore.addLineItem(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+//        productDataStore.addLineItem(new Product("A Game of Thrones", 6.62f, "USD", "Dragons, love and incest", sf, martin));
+//        productDataStore.addLineItem(new Product("Fire and Blood", 16.73f, "USD", "A Targaryen History", sf, martin));
+//        productDataStore.addLineItem(new Product("Ghosts of Gotham", 5.99f, "USD", "Mystery and magic", sf, shaefer));
+//        productDataStore.addLineItem(new Product("Lost and Found", 2.66f, "USD", "Love novel", romance, steel));
+//        productDataStore.addLineItem(new Product("Fairytale", 2.99f, "USD", "Love novel again", romance, steel));
+//        productDataStore.addLineItem(new Product("The Notebook", 7.88f, "USD", "La La Land was based on this", romance, sparks));
+//        productDataStore.addLineItem(new Product("See me", 8.16f, "USD", "Rich in emotion and fueled with suspense", romance, sparks));
+//        productDataStore.addLineItem(new Product("Harry Potter", 69.15f, "USD", "The Illustrated Collection", teens, rowling));
+//        productDataStore.addLineItem(new Product("ASUS Vivobook K570ZD", 493.37f, "USD", "Full Hd", laptop, asus));
+//        productDataStore.addLineItem(new Product("ASUS Whole Home Dual-Band AiMesh Router", 134.99f, "USD", "Mesh Wifi System", router, asus));
+//        productDataStore.addLineItem(new Product("ASUS C302CA-DHM4 Chromebook Flip", 465.00f, "USD", "12.5-inch Touchscreen", laptop, asus));
 //
 //    }
 //}
