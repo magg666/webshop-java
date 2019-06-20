dfa6718db591459e27733ba0ab3746f8cfdc0e8a
magg666 <mw23127@gmail.com>
2019-06-20 23:25:52 +0200

    added hashing password

--- src/main/java/com/codecool/shop/util/HashPassword.java
+++ src/main/java/com/codecool/shop/util/HashPassword.java
@@ -1,8 +1,33 @@
 package com.codecool.shop.util;
+import org.mindrot.jbcrypt.BCrypt;
 
 /**
  * This class contains methods for hashing password and compare
  * un-hashed and hashed strings
  */
+
 public class HashPassword {
+    /**
+     * Method to hash plain string     *
+     * @param password - password to hash
+     * @return String - hashed password
+     */
+    public static String getHashed(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    /**Check that an unencrypted password matches one that has
+     * previously been hashed
+     * @param candidate - String to check
+     * @param hashed - hashed String to compare
+     * @return boolean
+     */
+    public static boolean isMatching(String candidate, String hashed) {
+        return (BCrypt.checkpw(candidate, hashed));
+    }
 }
+
+
+
+
+
