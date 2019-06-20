8e0e84dbe602169e16bcf1ef7e3b2d171d6a66e6
magg666 <mw23127@gmail.com>
2019-06-21 01:29:00 +0200

    temporary disabled some functions

--- src/main/java/com/codecool/shop/controller/ConfirmationServlet.java
+++ src/main/java/com/codecool/shop/controller/ConfirmationServlet.java
@@ -4,16 +4,12 @@ import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.OrderDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Modifier;
 import java.util.Map;
 
 @WebServlet(urlPatterns = {"/thank-you"})
@@ -27,27 +23,27 @@ public class ConfirmationServlet extends MainServlet {
 
         Map<String, Object> additionalVariables = makeMapOfOrderVariables();
         ;
-        additionalVariables.put("name", session.getAttribute("name"));
-        additionalVariables.put("address", session.getAttribute("address"));
-        additionalVariables.put("email", session.getAttribute("email"));
+//        additionalVariables.put("name", session.getAttribute("name"));
+//        additionalVariables.put("address", session.getAttribute("address"));
+//        additionalVariables.put("email", session.getAttribute("email"));
 
 
-        try {
-            String orderId = String.valueOf(order.getOrder().getId());
-            String orderPrice = order.getOrder().getFormattedPrice();
-            String fileName = "No" + orderId + "Total" + orderPrice + ".json";
-            FileWriter writer = new FileWriter(fileName);
-            Gson gson = new GsonBuilder()
-                    .serializeNulls()
-                    .serializeSpecialFloatingPointValues()
-                    .excludeFieldsWithModifiers(Modifier.TRANSIENT)
-                    .setPrettyPrinting()
-                    .create();
-            gson.toJson(order.getOrder(), writer);
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            String orderId = String.valueOf(order.getOrder().getId());
+//            String orderPrice = order.getOrder().getFormattedPrice();
+//            String fileName = "No" + orderId + "Total" + orderPrice + ".json";
+//            FileWriter writer = new FileWriter(fileName);
+//            Gson gson = new GsonBuilder()
+//                    .serializeNulls()
+//                    .serializeSpecialFloatingPointValues()
+//                    .excludeFieldsWithModifiers(Modifier.TRANSIENT)
+//                    .setPrettyPrinting()
+//                    .create();
+//            gson.toJson(order.getOrder(), writer);
+//            writer.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
         order.clearOrder();
 
